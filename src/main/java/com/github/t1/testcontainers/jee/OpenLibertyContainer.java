7809ff571a503aa325126917025f080c4754f622
Rüdiger zu Dohna <snackbox@sinntr.eu>
2019-12-05 16:18:22 +0100

    Open Liberty: use `dropins` folder instead of `apps`... makes life much easier

--- src/main/java/com/github/t1/testcontainers/jee/OpenLibertyContainer.java
+++ src/main/java/com/github/t1/testcontainers/jee/OpenLibertyContainer.java
@@ -1,45 +1,17 @@
 package com.github.t1.testcontainers.jee;
 
-import lombok.SneakyThrows;
 import org.testcontainers.containers.wait.strategy.LogMessageWaitStrategy;
-import org.testcontainers.utility.MountableFile;
-
-import java.io.IOException;
-import java.net.URI;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
-import static java.nio.charset.StandardCharsets.UTF_8;
-import static java.nio.file.StandardOpenOption.APPEND;
 
 public class OpenLibertyContainer extends JeeContainer {
     private static final String SERVER_PATH = "/opt/ol/wlp/usr/servers/defaultServer";
-    public static final String APPS_PATH = SERVER_PATH + "/apps/";
-
-    private final Path serverXml = tempServerXml();
-
-    @SneakyThrows(IOException.class)
-    private static Path tempServerXml() {
-        return Files.createTempDirectory(null).resolve("server.xml");
-    }
+    public static final String DROPINS_PATH = SERVER_PATH + "/dropins/";
 
     public OpenLibertyContainer() { this(null); }
 
     public OpenLibertyContainer(String tag) {
         super(tagged("open-liberty", tag));
-        setContainerDeploymentPath(APPS_PATH);
+        setContainerDeploymentPath(DROPINS_PATH);
         addExposedPort(9080);
-        withCopyFileToContainer(MountableFile.forHostPath(serverXml), SERVER_PATH + "/server.xml");
         waitingFor(new LogMessageWaitStrategy().withRegEx(".*CWWKZ0001I: Application .* started.*"));
-    }
-
-    @SneakyThrows(IOException.class)
-    @Override public JeeContainer withDeployment(URI deployable) {
-        super.withDeployment(deployable);
-        Files.copy(OpenLibertyContainer.class.getResourceAsStream("/openliberty_server.xml"), serverXml);
-        Files.write(serverXml, (""
-            + "    <application id=\"app_war\" type=\"war\" location=\"" + containerPath() + "\" context-root=\"/" + webContext() + "\" />\n"
-            + "</server>\n").getBytes(UTF_8), APPEND);
-        return self();
     }
 }
