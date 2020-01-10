3e384635b50af6fde2cb945cf9a7ce10a50b6997
Rüdiger zu Dohna <snackbox@sinntr.eu>
2020-01-10 14:52:12 +0100

    write openliberty server.xml including the microProfile feature

--- src/main/java/com/github/t1/testcontainers/jee/OpenLibertyContainer.java
+++ src/main/java/com/github/t1/testcontainers/jee/OpenLibertyContainer.java
@@ -1,14 +1,18 @@
 package com.github.t1.testcontainers.jee;
 
 import org.testcontainers.containers.wait.strategy.LogMessageWaitStrategy;
+import org.testcontainers.utility.MountableFile;
 
 public class OpenLibertyContainer extends JeeContainer {
+    private static final String SERVER_PATH = "/opt/ol/wlp/usr/servers/defaultServer";
+
     public OpenLibertyContainer() { this(null); }
 
     public OpenLibertyContainer(String tag) {
         super(tagged("open-liberty", tag));
-        setContainerDeploymentPath("/opt/ol/wlp/usr/servers/defaultServer/dropins/");
+        setContainerDeploymentPath(SERVER_PATH + "/dropins/");
         addExposedPort(9080);
+        withCopyFileToContainer(MountableFile.forClasspathResource("/openliberty_server.xml"), SERVER_PATH + "/server.xml");
         waitingFor(new LogMessageWaitStrategy().withRegEx(".*CWWKZ0001I: Application .* started.*"));
     }
 }
