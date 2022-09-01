bee289ec4d7aab1d42d2277fe9f5e3d99911eafe
Ruediger zu Dohna <snackbox@sinntr.eu>
2022-09-01 11:05:44 +0200

    update logback; reenable payara tests

--- src/test/java/test/it/Payara.java
+++ src/test/java/test/it/Payara.java
@@ -1,6 +1,5 @@
 package test.it;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
 
@@ -9,7 +8,6 @@ import java.lang.annotation.Retention;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Tag("payara")
-@Disabled("broken also on GitHub actions")
 @DisabledIfSystemProperty(named = "os.arch", matches = "aarch64", disabledReason =
     "payara/server-full currently doesn't support arm64, and the emulation on M1 is too slow and brittle.")
 @Retention(RUNTIME)
